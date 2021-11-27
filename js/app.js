65fd24f80ec1bba8eb73a9f67ff4fd2d6f87544c
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2021-11-27 11:05:34 +0500

    add images
    add background images and styles for sections + add fullpage.js library

---
+++ js/app.js
@@ -0,0 +1,30 @@
+new fullpage('#fullpage', {
+    //options here
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+    menu: '#myMenu',
+    // autoScrolling: true,
+    // scrollHorizontally: true,
+    css3: true,
+    scrollingSpeed: 700,
+    navigation: true,
+    navigationTooltips: ['firstSlide', 'secondSlide'],
+    easing: 'easeInOutCubic',
+    easingcss3: 'ease',
+    keyboardScrolling: true,
+    animateAnchor: true,
+    recordHistory: true,
+});
+
+//methods
+fullpage_api.setAllowScrolling(true);
+
+window.onload = function() {
+    let images = ["../img/1.jpg", "../img/2.jpg", "../img/3.jpg", "../img/4.jpg", ];
+    let ban = document.querySelectorAll("#ban");
+    ban[0].style.backgroundImage = `url('${images[0]}')`
+    ban[1].style.backgroundImage = `url('${images[1]}')`
+    ban[2].style.backgroundImage = `url('${images[2]}')`
+    ban[2].style.backgroundPosition = `center bottom`
+    ban[3].style.backgroundImage = `url('${images[3]}')`
+    ban[3].style.backgroundPosition = `top`
+}
